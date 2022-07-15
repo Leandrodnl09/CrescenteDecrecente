5c1e293bd4487a9e11bf7aec026c5f16c634b61d
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-07-15 13:23:34 -0300

    CrescenteDecrescente

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Testando exercício "Crescente ou decrescente"
+
+string[] Num = Console.ReadLine().Split(' ');
+int X = int.Parse(Num[0]);
+int Y = int.Parse(Num[1]);
+
+while (X != Y)
+{
+    if (X > Y)
+    {
+        Console.WriteLine("Decrescente");
+    }
+    else
+    {
+        Console.WriteLine("Crescente");
+    }
+Num = Console.ReadLine().Split(' ');
+X = int.Parse(Num[0]);
+Y = int.Parse(Num[1]);
+}
